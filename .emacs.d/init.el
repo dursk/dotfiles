046e9d1af4571c3db7a13a8d7c61907a38e0c171
Matthew Madurski <mmadurski@managedbyq.com>
2016-04-15 12:27:47 -0400

    gpg keys yo

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -232,8 +232,9 @@
   :config
   (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
   (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
-  (setq js2-basic-offset 2)
-  )
+  (setq js2-basic-offset 2))
+
+(use-package markdown-mode)
 
 (use-package web-mode
   :config
