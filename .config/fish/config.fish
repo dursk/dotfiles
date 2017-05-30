b3abaa95c4cc301c8a1d3609cceb691768d8a625
Matthew Madurski <matt@matthewmadurski.com>
2017-05-30 16:11:40 -0400

    docker-compose

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -128,3 +128,7 @@ eval (python -m virtualfish)
 if set -q VIRTUAL_ENV
     echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
 end
+
+function dc
+    docker-compose $argv
+end
